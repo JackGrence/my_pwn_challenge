16667a1d38fb30417c29f89748ca76b7ab89515a
JackGrence <green1181772@gmail.com>
2018-02-22 16:30:55 +0800

    Reverse if conditional

--- stack_based/bof_basic/bof_basic.c
+++ stack_based/bof_basic/bof_basic.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
     int fail = 0;
     int fd;
     int len = 0;
-    if(!fail)
+    if(fail)
     {
         fd = open("flag.txt", 0);
         len = read(fd, overflowme, 128);
